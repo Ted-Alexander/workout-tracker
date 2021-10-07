93126ec9022f6a7569b547b2753b2c5aac92a3ed
Ted-Alexander <83848173+Ted-Alexander@users.noreply.github.com>
2021-10-07 16:55:34 -0400

    removed accidental duplicate files

--- server.js
+++ server.js
@@ -16,7 +16,17 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 // routes
+// app.use(require("./routes/api.js"));
 app.use(require("./routes/index.js"));
+
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(__dirname + "/public/exercise.html");
+  });
+
+app.get("/stats", (req, res) => {
+    res.sendFile(__dirname + "/public/stats.html");
+  });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
