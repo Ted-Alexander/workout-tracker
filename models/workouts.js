dbe61d5fc9b1f3cf1254952712b4641c1a69acb4
Ted-Alexander <83848173+Ted-Alexander@users.noreply.github.com>
2021-10-07 16:39:27 -0400

    server works now

--- models/workouts.js
+++ models/workouts.js
@@ -1,35 +1,49 @@
 const mongoose = require("mongoose");
 
-Workout.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-      },
-      PlanName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      creator: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      guest: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      schedule: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      cancelled: {
-        type: DataTypes.TINYINT,
-        defaultValue: 0,
-        allowNull: false,
-      },
-    },
-  );
+const Schema = mongoose.Schema;
+
+
   
+const workoutsSchema = new Schema({
+    
+    // isCardio: {
+    //     type: Boolean,
+    // },
+    // cardioName: {
+    //   name: String,
+    //   required: "Enter a name for workout",
+    // },
+    // Distance: {
+    //   type: Number,
+    //   required: "Enter a distance",
+    // },
+    //   Duration: {
+    //   type: Number,
+    // },
+    // Name: {
+    //   name: String,
+    //   trim: true,
+    //   required: "Enter a name for workout",
+    // },
+    // Weight: {
+    //   type: Number,
+    //   trim: true,
+    // },
+    // Sets:{
+    //   type: Number,
+    //   trim: true,
+    // },
+    // Reps:{
+    //   type: Number,
+    //   trim: true,
+    // },
+    // ResistanceDuration: {
+    //   type: Number,
+    //   trim: true,
+    // },
+  }  
+);
+
+const Workout = mongoose.model("Workout", workoutsSchema);
 
 module.exports = Workout;
--- routes/api/api-routes.js
+++ routes/api/api-routes.js
@@ -2,3 +2,7 @@ const router = require("express").Router();
 const Workout = require("../../models/workouts.js");
 
 router.post("/workouts", )
+
+
+
+module.exports = router;
