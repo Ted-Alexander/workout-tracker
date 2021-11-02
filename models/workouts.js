a2629aee2e14f7ae2fb6f0eb89fa23bd5b56658f
Ted-Alexander <83848173+Ted-Alexander@users.noreply.github.com>
2021-11-01 21:56:28 -0400

    fixed seed

--- models/workouts.js
+++ models/workouts.js
@@ -3,50 +3,50 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 
-  
+
 const workoutsSchema = new Schema({
-  
+
   day: {
     type: Date,
     default: Date.now
   },
   exercises: [
-  
-  
-  {
-    
-    type: {
+
+
+    {
+
+      type: {
         type: String,
         required: "Resistance or Cardio",
-    },
-    name: {
-      type: String,
-      trim: true,
-      required: "Enter a name for workout",
-    },
-    distance: {
-      type: Number,
-      required: "Enter a distance",
-    },
-    duration: {
-      type: Number,
-    },
-    weight: {
-      type: Number,
-      trim: true,
-    },
-    sets:{
-      type: Number,
-      trim: true,
-    },
-    reps:{
-      type: Number,
-      trim: true,
-    },
-    
-  
-  }  
-],
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for workout",
+      },
+      distance: {
+        type: Number,
+        required: "Enter a distance",
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+        trim: true,
+      },
+      sets: {
+        type: Number,
+        trim: true,
+      },
+      reps: {
+        type: Number,
+        trim: true,
+      },
+
+
+    }
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutsSchema);
